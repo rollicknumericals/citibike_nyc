6845e04dfe8788c00ec8c8e7a342234342fc45ca
rollicknumericals <castlematt15@gmail.com>
2025-06-05 16:01:31 -0800

    fixed minor typos in streamlit text

--- st_dashboard_citibike3.py
+++ st_dashboard_citibike3.py
@@ -36,9 +36,9 @@ df_line = pd.read_csv('2 Data/Prepped Data/reduced_data_for_plot.csv')
 ### Intro page
 
 if page == "Intro page":
-    st.markdown("#### This dashboard aims at assessing the current logistics model of bike distribution across the city, and identifying expansion opportunities.")
-    st.markdown("There are many positive aspects to CitiBike NYC's existing program. It's a cost effective means of transportation. It has thousands of docking stations aiding in convenient access to most any destination in the city. It also promotes health and fitness.") 
-    st.markdown("However, it faces operational challenges as well. Certain stations are full to the point that some riders are unable to return their bikes. Some stations are empty, or only contain broken bikes so riders can't rent when and where they need to. Rebalancing, maintenance, and rising costs are also potential problems.") 
+    st.markdown("#### This dashboard aims at assessing the current logistics model of bike distribution across the city and identifying expansion opportunities.")
+    st.markdown("There are many positive aspects to CitiBike NYC's existing program. It's a cost-effective means of transportation. It has thousands of docking stations aiding in convenient access to most any destination in the city. It also promotes health and fitness.") 
+    st.markdown("However, it currently faces some operational challenges. Certain stations are full to the point that some riders are unable to return their bikes. Some stations are empty, or only contain broken bikes so riders can't rent when and where they need to. Rebalancing, maintenance, and rising costs are also potential problems.") 
     st.markdown("This analysis will look at the potential reasons behind these. The dashboard is separated into 4 sections:")
     st.markdown("- Most popular stations")
     st.markdown("- Weather component and bike usage")
@@ -136,7 +136,7 @@ elif page == 'Most popular stations':
 
     # Step 8: Display the bar chart
     st.plotly_chart(fig, use_container_width=True)
-    st.markdown("From the bar chart it is clear that some start stations are more popular than others. Summer is the busiest season, with nearly three times the total rides compared to winter, the least busy season. The top 20 start stations account for just over 6% of all rides in the city, year-round. The top 3 busiest start stations throughout the year, are W 21 St & 6 Ave, West St & Chambers St, and Broadway & W 58 St.") 
+    st.markdown("The bar chart makes it clear that some start stations are more popular than others. Summer is the busiest season, with nearly three times the total rides compared to winter, the least busy season. The top 20 start stations account for just over 6% of all rides in the city, year-round. The top 3 busiest start stations throughout the year, are W 21 St & 6 Ave, West St & Chambers St, and Broadway & W 58 St.") 
     st.markdown("It's well known that 6th Avenue is a top shopping spot in NYC. There are also several corporate headquarters located there. The World Trade Center, 9/11 Memorial and Museum, Oculus - a major a transportation hub, as well as Hudson River Park, are major attractions near West St & Chambers St. Central Park, and Rockefeller Center are within walking distance of Broadway and W 58th St.")
 
 elif page == 'Interactive map with aggregated bike trips': 
@@ -154,11 +154,11 @@ elif page == 'Interactive map with aggregated bike trips':
     ## Show in webpage
     st.markdown("<h2 style='margin-bottom: -20px;'>Aggregated CitiBike Trips in NYC</h2>", unsafe_allow_html=True)
     st.components.v1.html(html_data, height=800)  # Adjusted height to 800
-    st.markdown("#### Using the filter on the left hand side of the map we can check whether the most popular start stations also appear in the most popular trips.")
+    st.markdown("#### Using the filter on the left-hand side of the map we can check whether the most popular start stations also appear in the most popular trips.")
     st.markdown("Within the most common trips made, there are few end-station points, and many start-station points. The most popular end points are near popular areas in, and outside the city.")
-    st.markdown("11th & West 27th street is among the most popular destinations. This is near The High Line, which is an elevated park. Built on former railway tracks, it offers unique views of the city. Also nearby are: Chelsea Market, a popular food hall; Hudson Yards, large-scale development featuring the Vessel - a unique interactive art structure; The Museum of Modern Art (MoMA); Rockefeller Center. These are popular spots for both locals and tourists.")
-    st.markdown("Newport parkway, which is actually located in New Jersey, offers views of the New York City skyline. Notable attractions nearby include Pier-A Park, the Hoboken Waterfront, and Newport Centre Mall. This area is also known for its scenic walkings, and boat tours along the Hudson River.")
-    st.markdown("4th & Jackson street is near Jackson Avenue Station, which is served by the 2 train at all hours.")
+    st.markdown("11th & West 27th street is among the most popular destinations. This is near The High Line, which is an elevated park. Built on former railway tracks, it offers unique views of the city. Also nearby are Chelsea Market, a popular food hall; Hudson Yards, large-scale development featuring the Vessel - a unique interactive art structure; The Museum of Modern Art (MoMA); Rockefeller Center. These are popular spots for both locals and tourists.")
+    st.markdown("Newport parkway, which is technicaally located in New Jersey, offers views of the New York City skyline. Notable attractions nearby include Pier-A Park, the Hoboken Waterfront, and Newport Centre Mall. This area is also known for its scenic walking paths, and boat tours along the Hudson River.")
+    st.markdown("4th & Jackson Street is near Jackson Avenue Station, which is served by the '2 train' at all hours.")
 
 else:
     
@@ -166,8 +166,8 @@ else:
     bikes = Image.open("tom-dillon-L9OHAHs41Lk-unsplash.jpg")  #source: https://https://unsplash.com/photos/woman-in-black-jacket-riding-blue-motorcycle-on-road-during-daytime-L9OHAHs41Lk
     st.image(bikes)
     st.markdown("### Our analysis has shown that CitiBikes should focus on the following objectives moving forward:")
-    st.markdown("- Ensure bikes are fully stocked in the busiest stations from mid-April through June. Then, to avoid overcrowding, cut back the fleet by the amount of units that need upkeep and maintenance during July and the first weeks August to guarantee we have the most bikes available for our busiest month-long period from late August to late September. Then maintain as full and functional a fleet as possible through mid-November. The dropping temperatures in early fall don't necessarily translate into less demand for bikes.")
+    st.markdown("- Ensure bikes are fully stocked in the busiest stations from mid-April through June. Then, to avoid overcrowding, cut back the fleet by the number of units that need upkeep and maintenance during July and the first weeks August to guarantee we have the most bikes available for our busiest month-long period from late August to late September. Then maintain as full and functional a fleet as possible through mid-November. The dropping temperatures in early fall don't necessarily translate into less demand for bikes.")
     st.markdown("- Create more stations near ferry terminals, major attractions, and parks, to both serve demand, and encourage more usage, in our established areas.")
-    st.markdown("- Scout new areas for more bike stations near subway entrances, and bus stops, to inscrease connectivity and supplemental transportation.")
+    st.markdown("- Scout new areas for more bike stations near subway entrances, and bus stops, to increase connectivity and supplemental transportation.")
     st.markdown("- Focus new stations in places with high foot traffic, like waterfront parkways, piers, and cultural hubs, as these are proven areas of interest for tourists and locals.")
     st.markdown("- Frequently redistribute bikes from heavily trafficked end stations to popular start stations to maintain a balanced circulation of bikes. This way we reduce overuse/underuse of portions of our fleet, therefore dispersing an even amount of wear and tear on the units. This will help lower maintenance costs.")
